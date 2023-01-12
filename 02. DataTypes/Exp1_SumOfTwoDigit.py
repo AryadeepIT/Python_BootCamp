2e8014e26ef4ebb472066193448244628b34799a
Aryadeep Chakraborty <AryadeepIT@gmail.com>
2023-01-12 19:21:35 +0530

    DataTypes - Python

---
+++ 02. DataTypes/Exp1_SumOfTwoDigit.py
@@ -0,0 +1,7 @@
+# Example input : 39
+# Output : 12      #3+9=12
+
+two_digit_number = input("Type a two digit number: ")
+first=((two_digit_number)[0])
+second=((two_digit_number)[1])
+print((int(first))+(int(second)))
