6242562f98ba784fe9377aae2c0737309eb7acfb
Aryadeep Chakraborty <AryadeepIT@gmail.com>
2023-02-04 23:08:02 +0530

    Pong game - Turtle

---
+++ 22. Project - Pong Arcade game/pong-game.py
@@ -0,0 +1,45 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+
+screen = Screen()
+
+# Setting the screen color-mode
+screen.colormode(255)
+
+# Changing the color of the pen the turtle carries
+screen.bgcolor(32, 32, 32)
+
+screen.setup(width=800, height=600)
+screen.title("Pong - Game")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    # Detect the collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+
+screen.exitonclick()
